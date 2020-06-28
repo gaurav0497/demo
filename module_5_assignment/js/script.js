735bae489d8e4cb1bc390fdb70ef9b0a96e1dce5
gaurav0497 <51540604+gaurav0497@users.noreply.github.com>
2020-06-28 21:39:32 +0530

    yessss

--- module_5_assignment/js/script.js
+++ module_5_assignment/js/script.js
@@ -115,7 +115,6 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-          var html = homeHtmlUrl;
           
           html =  insertProperty(homeHtml, "chosenCategoryShortName", chosenCategoryShortName);
           console.log(html);
